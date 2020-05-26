b411a4e6acfc420bc295a9eb6ed9e4860d69be5c
Psycho <laurentchervet@bluewin.ch>
2020-05-26 18:52:58 +0200

    Alice/PersonalPronoun support

--- ContextSensitive.py
+++ ContextSensitive.py
@@ -14,6 +14,7 @@ class ContextSensitive(AliceSkill):
 	def __init__(self):
 		self._history: Deque = deque(list(), 10)
 		self._sayHistory: Dict[str, Deque] = dict()
+		self._userSayHistory: Dict[str, Deque] = dict()
 		super().__init__()
 
 
@@ -29,7 +30,13 @@ class ContextSensitive(AliceSkill):
 
 	@IntentHandler('RepeatThis', isProtected=True)
 	def repeatThisIntent(self, session: DialogSession):
-		self.endDialog(session.sessionId, text=self.getLastChat(siteId=session.siteId))
+		if 'Pronoun' in session.slots:
+			if session.slotValue('Pronoun') == 'you':
+				self.endDialog(session.sessionId, text=self.getLastChat(siteId=session.siteId))
+			else:
+				self.endDialog(session.sessionId, text=self.getLastUserChat(siteId=session.siteId))
+		else:
+			self.endDialog(session.sessionId, text=self.getLastChat(siteId=session.siteId))
 
 
 	def addToMessageHistory(self, session: DialogSession) -> bool:
@@ -64,5 +71,16 @@ class ContextSensitive(AliceSkill):
 		self._sayHistory[siteId].append(text)
 
 
+	def addUserChat(self, text: str, siteId: str):
+		if siteId not in self._sayHistory:
+			self._userSayHistory[siteId] = deque(list(), 10)
+
+		self._userSayHistory[siteId].append(text)
+
+
 	def getLastChat(self, siteId: str) -> str:
 		return self._sayHistory[siteId][-1] if self._sayHistory.get(siteId) else self.randomTalk('nothing')
+
+
+	def getLastUserChat(self, siteId: str) -> str:
+		return self._userSayHistory[siteId][-2] if self._userSayHistory.get(siteId) else self.randomTalk('nothing')
